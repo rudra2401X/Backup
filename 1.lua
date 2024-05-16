9b7bc1d9dd8f5464b9ea7777cb41b66206b5231f
x2CPUz <89018587+x2CPUz@users.noreply.github.com>
2024-05-16 20:15:43 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,66 +1,6 @@
-
-
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SetUP.lua"))()
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local ImageButton = Instance.new("ImageButton")
-local TextButton = Instance.new("TextButton")
-
---Properties:
-
-ScreenGui.Parent = VG.GetProtecter()
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.BackgroundTransparency = 1.000
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.0260557048, 0, 0.650375962, 0)
-Frame.Size = UDim2.new(0, 395, 0, 136)
-
-ImageButton.Parent = Frame
-ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.BorderSizePixel = 0
-ImageButton.Position = UDim2.new(1, -(303 / 2), 0.5, -(263 / 2))
-ImageButton.Size = UDim2.new(0, 506, 0, 156)
-ImageButton.Image = "rbxassetid://17510231765"
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.BackgroundTransparency = 1.000
-TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.BorderSizePixel = 0
-TextButton.LayoutOrder = 2
-TextButton.Position = UDim2.new(2.23, -(303 / 2), 0.5, -(263 / 2))
-TextButton.Size = UDim2.new(0, 25, 0, 23)
-TextButton.Font = Enum.Font.SourceSansBold
-TextButton.LineHeight = 0.900
-TextButton.Text = "X"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextScaled = true
-TextButton.TextSize = 14.000
-TextButton.TextStrokeTransparency = 1.040
-TextButton.TextWrapped = true
-VG.SendNotification = function(Title, Text, Icon, Duration)
-    return StarterGui:SetCore("SendNotification", {Title = Title, Text = Text, Icon = Icon, Duration = Duration})
-end
-
-ImageButton.MouseButton1Click:Connect(function()
-    setclipboard("https://discord.gg/hackerclub")
-    VG.SendNotification("Copied to ClipBoard", 'Thank you', nil, 5)
+hookfunction(hookfunction,function(...)
+    return nil
 end)
-
-TextButton.MouseButton1Click:Connect(function()
-    ScreenGui:Destroy()
-end)
-
-wait(3)    
-    local message = require(game.ReplicatedStorage.Library.Client.Message)
-    message.Error("Welcome To Project Spectrum\nJoin With Us // discord.gg/hackerclub")
-    wait(1)
 
 warn("Anti AFK = Enabled :D")
 local vu = game:GetService("VirtualUser")
@@ -218,11 +158,11 @@ end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/AI.lua"))() -- Regular Globals
 VG.DisableConnection(Error)
 VG.DisableConnection(Idled)
- 
+
+local Things = Workspace:WaitForChild("__THINGS")
+local Active = Things.__INSTANCE_CONTAINER.Active
 local FishingGame = Player.PlayerGui._INSTANCES:WaitForChild("FishingGame")
 local Network = ReplicatedStorage:WaitForChild("Network")
-local Things = Workspace:WaitForChild("__THINGS")
-local Active = Things.__INSTANCE_CONTAINER:WaitForChild("Active")
 
 --------------- Fishing
 local Activated = function()
@@ -331,7 +271,6 @@ end
 local IsInFishingGame = function()
     return Player.PlayerGui._INSTANCES.FishingGame.Enabled
 end
-
 
 local LocalizationService = game:GetService("LocalizationService")
 local player = game.Players.LocalPlayer
@@ -530,6 +469,7 @@ do
                         end
                     }
                 )
+    
 
 local Toggle = Tabs.Fish:AddToggle("Fih", {Title = "Auto Fising [Smart AI]", Default = false})
 Toggle:OnChanged(function()
@@ -558,6 +498,9 @@ local Dropdown = Tabs.Fish:AddDropdown("e34", {Title = "Fishing Areas",Values = 
 Dropdown:OnChanged(function(Value)
     Method = Value
 end)
+
+local Quest = Tabs.Quest:AddSection("Coming Soon...")
+
 
 local Farm4141rqwqd4 = Tabs.Visuals:AddSection("Just Visuals")
 
@@ -992,3 +935,59 @@ Fluent:Notify(
 
 SaveManager:LoadAutoloadConfig()
 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SetUP.lua"))()
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ImageButton = Instance.new("ImageButton")
+local TextButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = VG.GetProtecter()
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.0260557048, 0, 0.650375962, 0)
+Frame.Size = UDim2.new(0, 395, 0, 136)
+
+ImageButton.Parent = Frame
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(1, -(303 / 2), 0.5, -(263 / 2))
+ImageButton.Size = UDim2.new(0, 506, 0, 156)
+ImageButton.Image = "rbxassetid://17510231765"
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.LayoutOrder = 2
+TextButton.Position = UDim2.new(2.23, -(303 / 2), 0.5, -(263 / 2))
+TextButton.Size = UDim2.new(0, 25, 0, 23)
+TextButton.Font = Enum.Font.SourceSansBold
+TextButton.LineHeight = 0.900
+TextButton.Text = "X"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextStrokeTransparency = 1.040
+TextButton.TextWrapped = true
+VG.SendNotification = function(Title, Text, Icon, Duration)
+    return StarterGui:SetCore("SendNotification", {Title = Title, Text = Text, Icon = Icon, Duration = Duration})
+end
+
+ImageButton.MouseButton1Click:Connect(function()
+    setclipboard("https://discord.gg/hackerclub")
+    VG.SendNotification("Copied to ClipBoard", 'Thank you', nil, 5)
+end)
+
+TextButton.MouseButton1Click:Connect(function()
+    ScreenGui:Destroy()
+end)
